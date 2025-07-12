902e44292bd76be7cc902f91117001ee8c6708f2
sagar-techy <142889336+sagar-techy@users.noreply.github.com>
2025-07-12 14:29:22 +0530

    Update Home.jsx

--- Frontend/src/views/home/Home.jsx
+++ Frontend/src/views/home/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "./Home.css";
+import "./home.css";
 
 const Home = () => {
   const navigate = useNavigate();
